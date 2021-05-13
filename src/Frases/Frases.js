f345956188e5bb6f42d86bd887bcbea76f5f1fd8
Emiliano Zuchovicki <emilianozuchovicki@hotmail.com>
2021-05-13 13:43:53 -0500

    Added Animo Diario (/*Working sin Estilo*/)

---
+++ src/Frases/Frases.js
@@ -0,0 +1,108 @@
+export const Frases = {
+
+    0:{
+        frase:" Las mejores y más bellas cosas en el mundo no se pueden ver, ni siquiera tocar, deben ser sentidas por el corazón.",
+        autor:"Helen Keller"
+    },
+
+    1:{
+        frase:" Las buenas cosas ocurren todos los días. Solo nos tenemos que dar cuenta de ellas.",
+        autor:"Anne Wilson Schaef"
+    },
+    2:{
+        frase:"Podemos orientarnos hacia la defensa, la seguridad o el miedo. Pero, en el lado opuesto, está la opción de crecimiento. Elegir el crecimiento en lugar del miedo doce veces al día, significa avanzar doce veces al día hacia la autorrealización.",
+        autor:"Abraham Maslow"
+    }, 
+    
+    3:{
+        frase:"Saber vivir es hacer lo mejor que podemos con lo que tenemos en el momento en que estamos.",
+        autor:"Alejandro Jodorowsky"
+    },
+    
+    4:{
+        frase:"No se puede atravesar el mar simplemente mirando el agua.",
+        autor:"Rabindranath Tagore"
+    },
+    5:{
+        frase:"Sigue a tu corazón, pero lleva contigo a tu cerebro.",
+        autor:"Alfred Adler"
+    },
+    6:{
+        frase:"El gran descubrimiento de mi generación es que los seres humanos pueden alterar sus vidas al alterar sus actitudes mentales.",
+        autor:"William James"
+    },
+    7:{
+        frase:"El primer paso de la sabiduría es echar la culpa a todo; el último, reconciliarse con todo.",
+        autor:"G. Lichtenberg"
+    },
+    8:{
+        frase:"No soy lo que me ha pasado. Soy lo que decido ser.",
+        autor:"Carl Gustav Jung"
+    },
+    9:{
+        frase:"No importa lo que ocurra, o lo malo que parezca todo hoy, la vida continúa, y mañana será mejor.",
+        autor:"Maya Angelou"
+    },
+    10:{
+        frase:"No es normal saber lo que queremos. Es un extraño y y difícil logro psicológico.",
+        autor:"Abraham Maslow"
+    },
+    11:{
+        frase:"Lo único que sé es que cualquier persona que quiera puede mejorar su bienestar.",
+        autor:"Carl Rogers"
+    },
+    12:{
+        frase:"Date cuenta de que ahora, en este momento, estás creando. Estás creando tu próximo momento basado en lo que sientes y piensas. Eso es lo real.",
+        autor:"Doc Childre"
+    },
+    13:{
+        frase:"Podríamos definir la terapia como una búsqueda de valor.",
+        autor:"Abraham Maslow"
+    },
+    14:{
+        frase:"En realidad, las personas autorrealizadas, las que han llegado a un alto nivel de madurez, salud y autosatisfacción, tienen tanto que enseñarnos que, a veces, casi parecen pertenecer a una especie diferente de seres humanos.",
+        autor:"Abraham Maslow"
+    },
+    15:{
+        frase:"No podemos adormecer selectivamente las emociones, cuando adormecemos las emociones dolorosas, también adormecemos las emociones positivas.",
+        autor:"Brene Brown"
+    },
+    16:{
+        frase:"Una persona, al descubrir que es amada por ser como es, no por lo que pretende ser, sentirá que merece respeto y amor.",
+        autor:"Carl Rogers"
+    },
+    17:{
+        frase:"Me doy cuenta de que si fuera estable, prudente y estático viviría en la muerte. Por consiguiente, acepto la confusión, la incertidumbre, el miedo y los altibajos emocionales, porque ese es el precio que estoy dispuesto a pagar por una vida fluida, perpleja y excitante.",
+        autor:"Carl Rogers"
+    },
+    18:{
+        frase:"La aceptación de uno mismo es el primer paso para la aceptación genuina de los demás.",
+        autor:"Carl Rogers"
+    },
+    19:{
+        frase:"El hombre se autorrealiza en la misma medida en que se compromete al cumplimiento del sentido de su vida.",
+        autor:"Viktor Frankl"
+    },
+    20:{
+        frase:"Lo que hay que cambiar en una persona es la consciencia de sí mismo.",
+        autor:"Abraham Maslow"
+    },
+    21:{
+        frase:"La buena vida es un proceso, no un estado del ser. Se trata de una dirección, no un destino.",
+        autor:"Carl Rogers"
+    },
+    22:{
+        frase:"El hombre nace libre, responsable y sin excusas.",
+        autor:"Jean Paul Sartre"
+    },
+    23:{
+        frase:"El hombre que se educa es aquel que aprende a aprender.",
+        autor:"Carl Rogers"
+    },
+    24:{
+        frase:"No podemos cambiar, no podemos alejarnos de lo que somos hasta que aceptamos lo que somos. Entonces el cambio parece llegar casi desapercibido.",
+        autor:"Carl Rogers"
+    },
+
+
+}
---
+++ src/pages/AnimoDiario.js
@@ -0,0 +1,46 @@
+import { View, Text } from 'react-native'
+import React, { useState, Component, useEffect } from 'react';
+import {Frases} from '../Frases/Frases'
+
+export default function Graficas({ navigation }) {
+
+const [Frase,setFrase] = useState()
+const [Autor,setAutor] = useState()
+let max = 0
+
+useEffect(function () {
+    Object.keys(Frases).forEach(()=>{
+        max+=1
+       })
+       let num = getRandomInt(0,max)
+       console.log(Frases[num])
+   
+       setAutor(Frases[num].autor)
+       setFrase(Frases[num].frase)
+   
+        
+    
+    
+    
+}, [])
+
+   
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+      }
+      
+
+
+    return (
+
+       <View>
+           <Text>{Frase}</Text>
+           <Text>{Autor}</Text>
+       </View>
+    )
+
+
+}
